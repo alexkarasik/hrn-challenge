86b4d06225e1f628ad5eb51a8c3a33f560da4b2e
kinkorea <alexkarasik89@gmail.com>
2017-09-23 15:09:57 -0400

    tried working on the button action, but was having some difficulty, so moved onto tablet sized media query. Moving onto phone size next

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+// $(".button").click(function() {
+//   $('.content').slideToggle().toggleClass('active');
+//   if ($('.content').hasClass('active')) {
+//     $('.header span').text('Collapse');
+//   } else {
+//     $('.header span').text('Expand');
+//   }
+// });
+// $('button').click(function(){
+// 	$(".header").trigger('click');
+// })
+
+
+  $("#btnExpand").click(function(){
+    $(".box").css('height', 'inherit');
+   });
