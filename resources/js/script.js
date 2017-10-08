c531acd43d274cd238614590c9189d3ee33f958e
kinkorea <alexkarasik89@gmail.com>
2017-10-08 08:15:41 -0400

    restart

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,22 +1,11 @@
-// $(".button").click(function() {
-//   $('.content').slideToggle().toggleClass('active');
-//   if ($('.content').hasClass('active')) {
-//     $('.header span').text('Collapse');
-//   } else {
-//     $('.header span').text('Expand');
-//   }
-// });
-// $('button').click(function(){
-// 	$(".header").trigger('click');
-// })
-
-
 $(document).ready(function() {
   $(".btnExpand").on('click', function() {
     if ($(".boxes .content").is(':visible')) {
       $(".boxes .content").hide();
+        $(".btnExpand").text("Compare Benefits");
     } else {
       $(".boxes .content").show();
+        $(".btnExpand").text("Close");
     }
   });
 });
